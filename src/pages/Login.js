92daec3560704d5ff5e9cd1348adcb8361ec9a66
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-31 15:44:15 -0300

    Page Login: create inputs and button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h1> Login </h1>
+        <input
+          type="text"
+          name="userMail"
+          // value={}
+          // onChange={}
+          id="login-user-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="text"
+          name="userPassword"
+          // value={}
+          // onChange={}
+          id="login-user-password"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+          // disabled={}
+          // onClick={}
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
