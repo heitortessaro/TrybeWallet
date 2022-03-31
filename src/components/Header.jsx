338a8a095392fdc6ae0fb735ca74c402f85a3098
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-03-31 18:13:04 -0300

    Add fields to the header. Some filds are fixed by now, but they will be dynamic as soon as next req are finesh.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
         HEADER
+        <br />
+        <span
+          data-testid="email-field"
+        >
+          {`Usuário ${email}`}
+        </span>
+        <br />
+        <span
+          data-testid="total-field"
+        >
+          0
+        </span>
+        <br />
+        <span
+          data-testid="header-currency-field"
+        >
+          BRL
+        </span>
       </div>
     );
   }
 }
 
-export default connect(null, null)(Header);
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
