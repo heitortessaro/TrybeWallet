02bcc0948d9ae2ca3430b3a7673fcf2fd17aa06f
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-01 14:33:45 -0300

    Create clean Forms component

---
+++ src/components/Forms.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Forms extends Component {
+  render() {
+    return (
+      <div>
+        Input
+      </div>
+    );
+  }
+}
+
+export default connect(null, null)(Forms);
